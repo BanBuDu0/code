a490746536b40360c89c8fef035222097d6c391e
syj <1171871069@qq.com>
2020-04-03 19:39:18 +0800

    update

--- leecode/src/com/huawei/TarjanMultiThread.java
+++ leecode/src/com/huawei/TarjanMultiThread.java
@@ -5,9 +5,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-
-public class Baseline {
+/**
+ * Find all simple cycles in a directed graph using Tarjan's algorithm.
+ * <p>
+ * Space complexity - O(E + V + S) where S is length of all cycles
+ * Time complexity - O(E*V(C+1) where C is total number of cycles
+ * <p>
+ * Reference - https://ecommons.cornell.edu/handle/1813/5941
+ * https://github.com/jgrapht/jgrapht/tree/master/jgrapht-core/src/main/java/org/jgrapht/alg/cycle
+ */
+public class TarjanMultiThread {
     static class Edge {
         private Vertex vertex1;
         private Vertex vertex2;
@@ -147,43 +157,101 @@ public class Baseline {
         }
     }
 
-    private Set<Vertex> visited = new HashSet<>();
-    private Deque<Vertex> pointStack = new LinkedList<>();
-    private Deque<Vertex> markedStack = new LinkedList<>();
-    private Set<Vertex> markedSet = new HashSet<>();
 
-    public List<List<Vertex>> findAllSimpleCycles(Graph graph) {
-        List<List<Vertex>> result = new ArrayList<>();
-        for (Vertex vertex : graph.getAllVertex()) {
-            findAllSimpleCycles(vertex, vertex, result);
-            visited.add(vertex);
-            while (!markedStack.isEmpty()) {
-                markedSet.remove(markedStack.pollFirst());
-            }
+    private List<Set<Vertex>> visiteds;
+    private List<Deque<Vertex>> pointStacks;
+    private List<Deque<Vertex>> markedStacks;
+    private List<Set<Vertex>> markedSets;
+    private List<List<List<Vertex>>> results;
+
+    private void reset(int num) {
+        visiteds = new ArrayList<>();
+        pointStacks = new ArrayList<>();
+        markedStacks = new ArrayList<>();
+        markedSets = new ArrayList<>();
+        results = new ArrayList<>();
+        for (int i = 0; i < num; ++i) {
+            visiteds.add(new HashSet<>());
+            pointStacks.add(new LinkedList<>());
+            markedStacks.add(new LinkedList<>());
+            markedSets.add(new HashSet<>());
+            results.add(new ArrayList<>());
         }
-        return result;
+//        visited = new HashSet<>();
+//        pointStack = new LinkedList<>();
+//        markedStack = new LinkedList<>();
+//        markedSet = new HashSet<>();
     }
 
-    private boolean findAllSimpleCycles(Vertex start, Vertex current, List<List<Vertex>> result) {
+    public List<List<Vertex>> findAllSimpleCycles(Graph graph) {
+        reset(graph.getAllVertex().size());
+        ExecutorService executor = Executors.newFixedThreadPool(graph.getAllVertex().size());
+        List<Vertex> vertices = graph.getAllVertex();
+        for (int i = 0; i < vertices.size(); ++i) {
+            Vertex vertex = vertices.get(i);
+            Set<Vertex> visited = visiteds.get(i);
+            Deque<Vertex> pointStack = pointStacks.get(i);
+            Deque<Vertex> markedStack = markedStacks.get(i);
+            Set<Vertex> markedSet = markedSets.get(i);
+            List<List<Vertex>> result = results.get(i);
+            executor.submit(() -> {
+//                    System.out.println(Thread.currentThread().getId() + " " + Thread.currentThread().getName());
+                findAllSimpleCycles(vertex, vertex, result, visited, pointStack, markedStack, markedSet);
+                visited.add(vertex);
+                while (!markedStack.isEmpty()) {
+                    markedSet.remove(markedStack.pollFirst());
+                }
+            });
+        }
+
+//        for (Vertex vertex : graph.getAllVertex()) {
+//            //113ms
+////            System.out.println(1);
+//            findAllSimpleCycles(vertex, vertex, result);
+//            visited.add(vertex);
+//            while (!markedStack.isEmpty()) {
+//                markedSet.remove(markedStack.pollFirst());
+//            }
+//        }
+        executor.shutdown();
+        List<List<Vertex>> res = new ArrayList<>();
+        for (List<List<Vertex>> i : results) {
+            res.addAll(i);
+        }
+        return res;
+    }
+
+    private int count = 0;
+
+    private boolean findAllSimpleCycles(Vertex start, Vertex current, List<List<Vertex>> result,
+                                        Set<Vertex> visited, Deque<Vertex> pointStack,
+                                        Deque<Vertex> markedStack, Set<Vertex> markedSet) {
+        ++count;
+//        System.out.println("in loop: " + count);
         boolean hasCycle = false;
         pointStack.offerFirst(current);
         markedSet.add(current);
         markedStack.offerFirst(current);
 
         for (Vertex w : current.getAdjacentVertexes()) {
+
             if (!visited.contains(w)) {
                 if (w.equals(start)) {
+                    //如果深搜又搜到了初始点，即存在环
                     hasCycle = true;
+//                pointStack.offerFirst(w);
                     if (pointStack.size() > 2 && pointStack.size() < 8) {
                         List<Vertex> cycle = new ArrayList<>();
+                        //头 43210 尾
                         Iterator<Vertex> itr = pointStack.descendingIterator();
                         while (itr.hasNext()) {
                             cycle.add(itr.next());
                         }
+//                pointStack.pollFirst();
                         result.add(cycle);
                     }
                 } else if (!markedSet.contains(w)) {
-                    hasCycle = findAllSimpleCycles(start, w, result) || hasCycle;
+                    hasCycle = findAllSimpleCycles(start, w, result, visited, pointStack, markedStack, markedSet) || hasCycle;
                 }
             }
         }
@@ -196,8 +264,11 @@ public class Baseline {
         }
 
         pointStack.pollFirst();
+        --count;
+        System.out.println("exit loop: " + count);
         return hasCycle;
     }
+
 
     private Graph generate_data(String path) {
         Graph graph = new Graph();
@@ -216,14 +287,39 @@ public class Baseline {
         return graph;
     }
 
-
     public static void main(String[] args) {
         String data_path = "D:\\code\\leecode\\src\\com\\huawei\\data\\test_data.txt";
-        Baseline baseline = new Baseline();
-        Graph graph = baseline.generate_data(data_path);
+        TarjanMultiThread tarjan = new TarjanMultiThread();
+        Graph graph = tarjan.generate_data(data_path);
+//        System.out.println(graph.toString());
+
+//        graph.addEdge(1, 2);
+//        graph.addEdge(1, 8);
+//        graph.addEdge(2, 3);
+//        graph.addEdge(2, 7);
+//        graph.addEdge(3, 1);
+//        graph.addEdge(3, 2);
+//        graph.addEdge(3, 4);
+//        graph.addEdge(3, 6);
+//        graph.addEdge(4, 5);
+//        graph.addEdge(5, 2);
+//        graph.addEdge(6, 4);
+//        graph.addEdge(8, 9);
+//        graph.addEdge(9, 8);
+
+//        int vertex_num = graph.getAllVertex().size();
+
 
         long startTime = System.currentTimeMillis();
-        List<List<Vertex>> result = baseline.findAllSimpleCycles(graph);
+        List<List<Vertex>> result = tarjan.findAllSimpleCycles(graph);
+//        int count = 0;
+//        for (List<Vertex> cycle : result) {
+//            if (cycle.size() > 2 && cycle.size() < 8) {
+//                ++count;
+//                cycle.forEach(v -> System.out.print(v.getId() + " "));
+//                System.out.println();
+//            }
+//        }
         result.forEach(cycle -> {
             cycle.forEach(v -> System.out.print(v.getId() + " "));
             System.out.println();
@@ -233,6 +329,3 @@ public class Baseline {
         System.out.println(endTime - startTime + "ms");
     }
 }
-
-
-
