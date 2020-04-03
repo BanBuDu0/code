cf033494d3e1b6f97d26173a2cde80bb1285f6e2
syj <1171871069@qq.com>
2020-04-03 17:01:43 +0800

    update

---
+++ leecode/src/com/huawei/Baseline.java
@@ -0,0 +1,238 @@
+package com.huawei;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+
+public class Baseline {
+    static class Edge {
+        private Vertex vertex1;
+        private Vertex vertex2;
+        private int weight;
+
+        Edge(Vertex vertex1, Vertex vertex2, int weight) {
+            this.vertex1 = vertex1;
+            this.vertex2 = vertex2;
+            this.weight = weight;
+        }
+
+        Vertex getVertex1() {
+            return vertex1;
+        }
+
+        Vertex getVertex2() {
+            return vertex2;
+        }
+
+        int getWeight() {
+            return weight;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((vertex1 == null) ? 0 : vertex1.hashCode());
+            result = prime * result + ((vertex2 == null) ? 0 : vertex2.hashCode());
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "vertex1=" + vertex1
+                    + ", vertex2=" + vertex2 + ", weight=" + weight + "]";
+        }
+    }
+
+    static class Vertex {
+        long id;
+        private List<Vertex> adjacentVertex = new ArrayList<>();
+
+        Vertex(long id) {
+            this.id = id;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void addAdjacentVertex(Vertex v) {
+            adjacentVertex.add(v);
+        }
+
+        public String toString() {
+            return String.valueOf(id);
+        }
+
+        public List<Vertex> getAdjacentVertexes() {
+            return adjacentVertex;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + (int) (id ^ (id >>> 32));
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Vertex other = (Vertex) obj;
+            return id == other.id;
+        }
+    }
+
+    static class Graph {
+        private List<Edge> allEdges;
+        private Map<Long, Vertex> allVertex;
+
+        public Graph() {
+            allEdges = new ArrayList<>();
+            allVertex = new HashMap<>();
+        }
+
+        public void addEdge(long id1, long id2) {
+            addEdge(id1, id2, 0);
+        }
+
+        public void addEdge(long id1, long id2, int weight) {
+            Vertex vertex1;
+            if (allVertex.containsKey(id1)) {
+                vertex1 = allVertex.get(id1);
+            } else {
+                vertex1 = new Vertex(id1);
+                allVertex.put(id1, vertex1);
+            }
+            Vertex vertex2;
+            if (allVertex.containsKey(id2)) {
+                vertex2 = allVertex.get(id2);
+            } else {
+                vertex2 = new Vertex(id2);
+                allVertex.put(id2, vertex2);
+            }
+
+            Edge edge = new Edge(vertex1, vertex2, weight);
+            allEdges.add(edge);
+            vertex1.addAdjacentVertex(vertex2);
+
+        }
+
+        public List<Edge> getAllEdges() {
+            return allEdges;
+        }
+
+        public List<Vertex> getAllVertex() {
+            return new ArrayList<>(allVertex.values());
+        }
+
+
+        @Override
+        public String toString() {
+            StringBuilder buffer = new StringBuilder();
+            for (Edge edge : getAllEdges()) {
+                buffer.append(edge.getVertex1()).append(" ").append(edge.getVertex2()).append(" ").append(edge.getWeight());
+                buffer.append("\n");
+            }
+            return buffer.toString();
+        }
+    }
+
+    private Set<Vertex> visited = new HashSet<>();
+    private Deque<Vertex> pointStack = new LinkedList<>();
+    private Deque<Vertex> markedStack = new LinkedList<>();
+    private Set<Vertex> markedSet = new HashSet<>();
+
+    public List<List<Vertex>> findAllSimpleCycles(Graph graph) {
+        List<List<Vertex>> result = new ArrayList<>();
+        for (Vertex vertex : graph.getAllVertex()) {
+            findAllSimpleCycles(vertex, vertex, result);
+            visited.add(vertex);
+            while (!markedStack.isEmpty()) {
+                markedSet.remove(markedStack.pollFirst());
+            }
+        }
+        return result;
+    }
+
+    private boolean findAllSimpleCycles(Vertex start, Vertex current, List<List<Vertex>> result) {
+        boolean hasCycle = false;
+        pointStack.offerFirst(current);
+        markedSet.add(current);
+        markedStack.offerFirst(current);
+
+        for (Vertex w : current.getAdjacentVertexes()) {
+            if (!visited.contains(w)) {
+                if (w.equals(start)) {
+                    hasCycle = true;
+                    if (pointStack.size() > 2 && pointStack.size() < 8) {
+                        List<Vertex> cycle = new ArrayList<>();
+                        Iterator<Vertex> itr = pointStack.descendingIterator();
+                        while (itr.hasNext()) {
+                            cycle.add(itr.next());
+                        }
+                        result.add(cycle);
+                    }
+                } else if (!markedSet.contains(w)) {
+                    hasCycle = findAllSimpleCycles(start, w, result) || hasCycle;
+                }
+            }
+        }
+
+        if (hasCycle) {
+            while (!Objects.requireNonNull(markedStack.peekFirst()).equals(current)) {
+                markedSet.remove(markedStack.pollFirst());
+            }
+            markedSet.remove(markedStack.pollFirst());
+        }
+
+        pointStack.pollFirst();
+        return hasCycle;
+    }
+
+    private Graph generate_data(String path) {
+        Graph graph = new Graph();
+        File file = new File(path);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String temp;
+            while ((temp = br.readLine()) != null) {
+                String[] t = temp.split(",");
+                graph.addEdge(Long.parseLong(t[0]), Long.parseLong(t[1]));
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return graph;
+    }
+
+
+    public static void main(String[] args) {
+        String data_path = "D:\\code\\leecode\\src\\com\\huawei\\data\\test_data.txt";
+        Baseline baseline = new Baseline();
+        Graph graph = baseline.generate_data(data_path);
+
+        long startTime = System.currentTimeMillis();
+        List<List<Vertex>> result = baseline.findAllSimpleCycles(graph);
+        result.forEach(cycle -> {
+            cycle.forEach(v -> System.out.print(v.getId() + " "));
+            System.out.println();
+        });
+        System.out.println(result.size());
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime + "ms");
+    }
+}
+
+
+
--- leecode/src/com/huawei/Tarjan.java
+++ leecode/src/com/huawei/Tarjan.java
@@ -1,348 +1,196 @@
-package com.huawei;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.*;
-
-/**
- * Find all simple cycles in a directed graph using Tarjan's algorithm.
- * <p>
- * Space complexity - O(E + V + S) where S is length of all cycles
- * Time complexity - O(E*V(C+1) where C is total number of cycles
- * <p>
- * Reference - https://ecommons.cornell.edu/handle/1813/5941
- * https://github.com/jgrapht/jgrapht/tree/master/jgrapht-core/src/main/java/org/jgrapht/alg/cycle
- */
-public class Tarjan {
-    private Set<Vertex<Integer>> visited;
-    private Deque<Vertex<Integer>> pointStack;
-    private Deque<Vertex<Integer>> markedStack;
-    private Set<Vertex<Integer>> markedSet;
-
-    private void reset() {
-        visited = new HashSet<>();
-        pointStack = new LinkedList<>();
-        markedStack = new LinkedList<>();
-        markedSet = new HashSet<>();
-    }
-
-    public List<List<Vertex<Integer>>> findAllSimpleCycles(Graph<Integer> graph) {
-        reset();
-        List<List<Vertex<Integer>>> result = new ArrayList<>();
-        for (Vertex<Integer> vertex : graph.getAllVertex()) {
-            findAllSimpleCycles(vertex, vertex, result);
-            visited.add(vertex);
-            while (!markedStack.isEmpty()) {
-                markedSet.remove(markedStack.pollFirst());
-            }
-        }
-        return result;
-    }
-
-    private boolean findAllSimpleCycles(Vertex<Integer> start, Vertex<Integer> current, List<List<Vertex<Integer>>> result) {
-        boolean hasCycle = false;
-        pointStack.offerFirst(current);
-        markedSet.add(current);
-        markedStack.offerFirst(current);
-
-        for (Vertex<Integer> w : current.getAdjacentVertexes()) {
-            if (visited.contains(w)) {
-                continue;
-            } else if (w.equals(start)) {
-                hasCycle = true;
-                pointStack.offerFirst(w);
-                List<Vertex<Integer>> cycle = new ArrayList<>();
-                Iterator<Vertex<Integer>> itr = pointStack.descendingIterator();
-                while (itr.hasNext()) {
-                    cycle.add(itr.next());
-                }
-                pointStack.pollFirst();
-                result.add(cycle);
-            } else if (!markedSet.contains(w)) {
-                hasCycle = findAllSimpleCycles(start, w, result) || hasCycle;
-            }
-        }
-
-        if (hasCycle) {
-            while (!markedStack.peekFirst().equals(current)) {
-                markedSet.remove(markedStack.pollFirst());
-            }
-            markedSet.remove(markedStack.pollFirst());
-        }
-
-        pointStack.pollFirst();
-        return hasCycle;
-    }
-
-    public static void main(String[] args) {
-        Graph<Integer> graph = new Graph<>(true);
-        String data_path = "D:\\python\\huawei\\data\\test_data.txt";
-        File file = new File(data_path);
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String temp;
-            while ((temp = br.readLine()) != null) {//使用readLine方法，一次读一行
-                String[] t = temp.split(",");
-                graph.addEdge(Long.parseLong(t[0]), Long.parseLong(t[1]));
-            }
-            br.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Tarjan tarjan = new Tarjan();
-        long startTime = System.currentTimeMillis();
-        List<List<Vertex<Integer>>> result = tarjan.findAllSimpleCycles(graph);
-        int count = 0;
-        for (List<Vertex<Integer>> cycle : result) {
-            if (cycle.size() > 3 && cycle.size() < 9) {
-                ++count;
-                cycle.forEach(v -> System.out.print(v.getId() + " "));
-                System.out.println();
-            }
-        }
-        System.out.println(count);
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime + "ms");
-    }
-}
-
-
-class Edge<T> {
-    private boolean isDirected = false;
-    private Vertex<T> vertex1;
-    private Vertex<T> vertex2;
-    private int weight;
-
-    Edge(Vertex<T> vertex1, Vertex<T> vertex2) {
-        this.vertex1 = vertex1;
-        this.vertex2 = vertex2;
-    }
-
-    Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected, int weight) {
-        this.vertex1 = vertex1;
-        this.vertex2 = vertex2;
-        this.weight = weight;
-        this.isDirected = isDirected;
-    }
-
-    Edge(Vertex<T> vertex1, Vertex<T> vertex2, boolean isDirected) {
-        this.vertex1 = vertex1;
-        this.vertex2 = vertex2;
-        this.isDirected = isDirected;
-    }
-
-    Vertex<T> getVertex1() {
-        return vertex1;
-    }
-
-    Vertex<T> getVertex2() {
-        return vertex2;
-    }
-
-    int getWeight() {
-        return weight;
-    }
-
-    public boolean isDirected() {
-        return isDirected;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((vertex1 == null) ? 0 : vertex1.hashCode());
-        result = prime * result + ((vertex2 == null) ? 0 : vertex2.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Edge other = (Edge) obj;
-        if (vertex1 == null) {
-            if (other.vertex1 != null)
-                return false;
-        } else if (!vertex1.equals(other.vertex1))
-            return false;
-        if (vertex2 == null) {
-            if (other.vertex2 != null)
-                return false;
-        } else if (!vertex2.equals(other.vertex2))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Edge [isDirected=" + isDirected + ", vertex1=" + vertex1
-                + ", vertex2=" + vertex2 + ", weight=" + weight + "]";
-    }
-}
-
-class Vertex<T> {
-    long id;
-    private T data;
-    private List<Edge<T>> edges = new ArrayList<>();
-    private List<Vertex<T>> adjacentVertex = new ArrayList<>();
-
-    Vertex(long id) {
-        this.id = id;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public void addAdjacentVertex(Edge<T> e, Vertex<T> v) {
-        edges.add(e);
-        adjacentVertex.add(v);
-    }
-
-    public String toString() {
-        return String.valueOf(id);
-    }
-
-    public List<Vertex<T>> getAdjacentVertexes() {
-        return adjacentVertex;
-    }
-
-    public List<Edge<T>> getEdges() {
-        return edges;
-    }
-
-    public int getDegree() {
-        return edges.size();
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (int) (id ^ (id >>> 32));
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Vertex other = (Vertex) obj;
-        if (id != other.id)
-            return false;
-        return true;
-    }
-}
-
-class Graph<T> {
-
-    private List<Edge<T>> allEdges;
-    private Map<Long, Vertex<T>> allVertex;
-    boolean isDirected = false;
-
-    public Graph(boolean isDirected) {
-        allEdges = new ArrayList<Edge<T>>();
-        allVertex = new HashMap<Long, Vertex<T>>();
-        this.isDirected = isDirected;
-    }
-
-    public void addEdge(long id1, long id2) {
-        addEdge(id1, id2, 0);
-    }
-
-    //This works only for directed graph because for undirected graph we can end up
-    //adding edges two times to allEdges
-    public void addVertex(Vertex<T> vertex) {
-        if (allVertex.containsKey(vertex.getId())) {
-            return;
-        }
-        allVertex.put(vertex.getId(), vertex);
-        for (Edge<T> edge : vertex.getEdges()) {
-            allEdges.add(edge);
-        }
-    }
-
-    public Vertex<T> addSingleVertex(long id) {
-        if (allVertex.containsKey(id)) {
-            return allVertex.get(id);
-        }
-        Vertex<T> v = new Vertex<T>(id);
-        allVertex.put(id, v);
-        return v;
-    }
-
-    public Vertex<T> getVertex(long id) {
-        return allVertex.get(id);
-    }
-
-    public void addEdge(long id1, long id2, int weight) {
-        Vertex<T> vertex1 = null;
-        if (allVertex.containsKey(id1)) {
-            vertex1 = allVertex.get(id1);
-        } else {
-            vertex1 = new Vertex<T>(id1);
-            allVertex.put(id1, vertex1);
-        }
-        Vertex<T> vertex2 = null;
-        if (allVertex.containsKey(id2)) {
-            vertex2 = allVertex.get(id2);
-        } else {
-            vertex2 = new Vertex<T>(id2);
-            allVertex.put(id2, vertex2);
-        }
-
-        Edge<T> edge = new Edge<T>(vertex1, vertex2, isDirected, weight);
-        allEdges.add(edge);
-        vertex1.addAdjacentVertex(edge, vertex2);
-        if (!isDirected) {
-            vertex2.addAdjacentVertex(edge, vertex1);
-        }
-
-    }
-
-    public List<Edge<T>> getAllEdges() {
-        return allEdges;
-    }
-
-    public Collection<Vertex<T>> getAllVertex() {
-        return allVertex.values();
-    }
-
-    public void setDataForVertex(long id, T data) {
-        if (allVertex.containsKey(id)) {
-            Vertex<T> vertex = allVertex.get(id);
-            vertex.setData(data);
-        }
-    }
-
-    @Override
-    public String toString() {
-        StringBuffer buffer = new StringBuffer();
-        for (Edge<T> edge : getAllEdges()) {
-            buffer.append(edge.getVertex1() + " " + edge.getVertex2() + " " + edge.getWeight());
-            buffer.append("\n");
-        }
-        return buffer.toString();
-    }
-}
-
+//package com.huawei;
+//
+//import com.huawei.common.Graph;
+//import com.huawei.common.Vertex;
+//
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.*;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+///**
+// * Find all simple cycles in a directed graph using Tarjan's algorithm.
+// * <p>
+// * Space complexity - O(E + V + S) where S is length of all cycles
+// * Time complexity - O(E*V(C+1) where C is total number of cycles
+// * <p>
+// * Reference - https://ecommons.cornell.edu/handle/1813/5941
+// * https://github.com/jgrapht/jgrapht/tree/master/jgrapht-core/src/main/java/org/jgrapht/alg/cycle
+// */
+//public class Tarjan {
+//    private List<Set<Vertex<Integer>>> visiteds;
+//    private List<Deque<Vertex<Integer>>> pointStacks;
+//    private List<Deque<Vertex<Integer>>> markedStacks;
+//    private List<Set<Vertex<Integer>>> markedSets;
+//    private List<List<List<Vertex<Integer>>>> results;
+//
+//    private void reset(int num) {
+//        visiteds = new ArrayList<>();
+//        pointStacks = new ArrayList<>();
+//        markedStacks = new ArrayList<>();
+//        markedSets = new ArrayList<>();
+//        results = new ArrayList<>();
+//        for (int i = 0; i < num; ++i) {
+//            visiteds.add(new HashSet<>());
+//            pointStacks.add(new LinkedList<>());
+//            markedStacks.add(new LinkedList<>());
+//            markedSets.add(new HashSet<>());
+//            results.add(new ArrayList<>());
+//        }
+////        visited = new HashSet<>();
+////        pointStack = new LinkedList<>();
+////        markedStack = new LinkedList<>();
+////        markedSet = new HashSet<>();
+//    }
+//
+//    public List<List<Vertex<Integer>>> findAllSimpleCycles(Graph<Integer> graph) {
+//        reset(graph.getAllVertex().size());
+//        ExecutorService executor = Executors.newFixedThreadPool(graph.getAllVertex().size());
+//        List<Vertex<Integer>> vertices = graph.getAllVertex();
+//        for (int i = 0; i < vertices.size(); ++i) {
+//            Vertex<Integer> vertex = vertices.get(i);
+//            Set<Vertex<Integer>> visited = visiteds.get(i);
+//            Deque<Vertex<Integer>> pointStack = pointStacks.get(i);
+//            Deque<Vertex<Integer>> markedStack = markedStacks.get(i);
+//            Set<Vertex<Integer>> markedSet = markedSets.get(i);
+//            List<List<Vertex<Integer>>> result = results.get(i);
+//            executor.submit(new Runnable() {
+//                @Override
+//                public void run() {
+////                    System.out.println(Thread.currentThread().getId() + " " + Thread.currentThread().getName());
+//                    findAllSimpleCycles(vertex, vertex, result, visited, pointStack, markedStack, markedSet);
+//                    visited.add(vertex);
+//                    while (!markedStack.isEmpty()) {
+//                        markedSet.remove(markedStack.pollFirst());
+//                    }
+//                }
+//            });
+//        }
+//
+////        for (Vertex<Integer> vertex : graph.getAllVertex()) {
+////            //113ms
+//////            System.out.println(1);
+////            findAllSimpleCycles(vertex, vertex, result);
+////            visited.add(vertex);
+////            while (!markedStack.isEmpty()) {
+////                markedSet.remove(markedStack.pollFirst());
+////            }
+////        }
+//        executor.shutdown();
+//        List<List<Vertex<Integer>>> res = new ArrayList<>();
+//        for (List<List<Vertex<Integer>>> i : results) {
+//            res.addAll(i);
+//        }
+//        return res;
+//    }
+//
+//    private int count = 0;
+//
+//    private boolean findAllSimpleCycles(Vertex<Integer> start, Vertex<Integer> current, List<List<Vertex<Integer>>> result,
+//                                        Set<Vertex<Integer>> visited, Deque<Vertex<Integer>> pointStack,
+//                                        Deque<Vertex<Integer>> markedStack, Set<Vertex<Integer>> markedSet) {
+//        ++count;
+////        System.out.println("in loop: " + count);
+//        boolean hasCycle = false;
+//        pointStack.offerFirst(current);
+//        markedSet.add(current);
+//        markedStack.offerFirst(current);
+//
+//        for (Vertex<Integer> w : current.getAdjacentVertexes()) {
+//            if (visited.contains(w)) {
+//                continue;
+//            } else if (w.equals(start)) {
+//                //如果深搜又搜到了初始点，即存在环
+//                hasCycle = true;
+////                pointStack.offerFirst(w);
+//                if (pointStack.size() > 2 && pointStack.size() < 8) {
+//                    List<Vertex<Integer>> cycle = new ArrayList<>();
+//                    //头 43210 尾
+//                    Iterator<Vertex<Integer>> itr = pointStack.descendingIterator();
+//                    while (itr.hasNext()) {
+//                        cycle.add(itr.next());
+//                    }
+////                pointStack.pollFirst();
+//                    result.add(cycle);
+//                }
+//            } else if (!markedSet.contains(w)) {
+//                hasCycle = findAllSimpleCycles(start, w, result, visited, pointStack, markedStack, markedSet) || hasCycle;
+//            }
+//        }
+//
+//        if (hasCycle) {
+//            while (!markedStack.peekFirst().equals(current)) {
+//                markedSet.remove(markedStack.pollFirst());
+//            }
+//            markedSet.remove(markedStack.pollFirst());
+//        }
+//
+//        pointStack.pollFirst();
+//        --count;
+//        System.out.println("exit loop: " + count);
+//        return hasCycle;
+//    }
+//
+//
+//    private Graph<Integer> generate_data(String path) {
+//        Graph<Integer> graph = new Graph<>(true);
+//        File file = new File(path);
+//        try {
+//            BufferedReader br = new BufferedReader(new FileReader(file));
+//            String temp;
+//            while ((temp = br.readLine()) != null) {
+//                String[] t = temp.split(",");
+//                graph.addEdge(Long.parseLong(t[0]), Long.parseLong(t[1]));
+//            }
+//            br.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return graph;
+//    }
+//
+//    public static void main(String[] args) {
+//        String data_path = "D:\\code\\leecode\\src\\com\\huawei\\data\\test_data1.txt";
+//        Tarjan tarjan = new Tarjan();
+//        Graph<Integer> graph = tarjan.generate_data(data_path);
+////        System.out.println(graph.toString());
+//
+////        graph.addEdge(1, 2);
+////        graph.addEdge(1, 8);
+////        graph.addEdge(2, 3);
+////        graph.addEdge(2, 7);
+////        graph.addEdge(3, 1);
+////        graph.addEdge(3, 2);
+////        graph.addEdge(3, 4);
+////        graph.addEdge(3, 6);
+////        graph.addEdge(4, 5);
+////        graph.addEdge(5, 2);
+////        graph.addEdge(6, 4);
+////        graph.addEdge(8, 9);
+////        graph.addEdge(9, 8);
+//
+////        int vertex_num = graph.getAllVertex().size();
+//
+//
+//        long startTime = System.currentTimeMillis();
+//        List<List<Vertex<Integer>>> result = tarjan.findAllSimpleCycles(graph);
+////        int count = 0;
+////        for (List<Vertex<Integer>> cycle : result) {
+////            if (cycle.size() > 2 && cycle.size() < 8) {
+////                ++count;
+////                cycle.forEach(v -> System.out.print(v.getId() + " "));
+////                System.out.println();
+////            }
+////        }
+//        result.forEach(cycle -> {
+//            cycle.forEach(v -> System.out.print(v.getId() + " "));
+//            System.out.println();
+//        });
+//        System.out.println(result.size());
+//        long endTime = System.currentTimeMillis();
+//        System.out.println(endTime - startTime + "ms");
+//    }
+//}
